49bedadfc1e949903593e5336c3271e277e89e99
EC2 Default User <ec2-user@ip-172-31-31-33.ec2.internal>
2022-07-21 11:11:18 +0000

    終了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_19_120246) do
+ActiveRecord::Schema.define(version: 2022_07_08_032758) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,12 +43,7 @@ ActiveRecord::Schema.define(version: 2022_07_19_120246) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
-  end
-
-  create_table "post_images", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -64,7 +59,6 @@ ActiveRecord::Schema.define(version: 2022_07_19_120246) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
